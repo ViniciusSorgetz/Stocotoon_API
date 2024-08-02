a995ada6bb59c4834cea1ab7939db52647d9a34c
Rexzinhoo <viniciussorgetzalves@gmail.com>
2024-08-02 19:35:00 -0300

    Rota de listar mensagens agora mostra o nick do usuário

--- controllers/ChatController.js
+++ controllers/ChatController.js
@@ -1,5 +1,6 @@
 const Chat = require("../models/Chat");
 const Message = require("../models/Message");
+const User = require("../models/User");
 
 module.exports = class ChatController{
     
@@ -39,12 +40,23 @@ module.exports = class ChatController{
         try {
             const chat = await Chat.findOne({where: {id: ChatId}, raw: true});
             const messages = await Message.findAll({where: {ChatId: ChatId}, raw: true});
+            const updatedMessages = await Promise.all(messages.map(async message => {
+                const user = await User.findOne({where: {id: message.UserId}, raw: true});
+                return {
+                    id: message.id,
+                    nick: user.name,
+                    content: message.content,
+                    createdAt: message.createdAt,
+                    updatedAt: message.updatedAt
+                }
+            }));
             return res.status(200).json({
                 ...chat,
-                messages: [...messages]
+                messages: [...updatedMessages]
             });
         } 
         catch (error) {
+            console.log(error);
             return res.status(500).json({
                 message: "Erro ao listar mensagens. Tente novamente mais tarde."
             })
